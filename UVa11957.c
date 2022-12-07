9803f08b506bbb5d31cdf07db2adb22d46957fc0
HaoTzuH <119925963+HaoTzuH@users.noreply.github.com>
2022-12-07 20:07:04 +0800

    Update UVa11957.c

--- UVa11957.c
+++ UVa11957.c
@@ -3,13 +3,16 @@
 //第二個以後是那組地圖的模樣，B障礙物不能過且不動，W是要移動的
 //(x+1,y+1)  (x-1,y+1)
 //(x+2,y+2)  (x-2,y+2)
+//紀錄有走過的
 
 #include<stdio.h>
+#include<stdlib.h>
 int main()
 {
     int testnumber=0, maplen=0, px=0, py=0, Case = 1;
-    int i = 0, j = 0, start[101][101];
-    char mapp[101][101];
+    int i = 0, j = 0, start[105][105]={};
+    char mapp[105][105];
+	int cases = 0;
     scanf("%d", &testnumber);
     while(testnumber--)
     {
@@ -17,21 +20,24 @@ int main()
         scanf("%d", &maplen);
         for(i = 0; i < maplen; i++)
         {
-            for(j = 0; j < maplen; j++)
-            {
-                 scanf("%s", &mapp[i][j]);
-                 //if(mapp[i][j] == 'W')
-                 //{
-                       //px = i;
-                       //py = j;
-                // }
-            }
+            ///for(j = 0; j < maplen; j++)
+            //{
+                 scanf("%s", mapp[i]);
+            //}
         }
         //--------
         //start[px][py]=1;
-        for(i = testnumber-1; i >= 0; i--)
+		
+		for(i = 0 ; i <105;i++)
+		{
+			for(j = 0 ;j<105;j++)
+			{
+				start[i][j]=0;
+			}
+		}
+        for(i = maplen-1; i >= 0; i--)
         {
-            for(j = 0; j < testnumber; j++)
+            for(j = 0; j < maplen; j++)
             {
                 if(mapp[i][j] == 'W')
                 {
@@ -42,24 +48,24 @@ int main()
                 {
                     continue;
                 }
-                if(i+1 < testnumber && j+1 <testnumber && mapp[i][j] !='B')
+                if(i+1 < maplen && j+1 <maplen && mapp[i+1][j+1] !='B')
                 {
                    start[i][j]+=start[i+1][j+1];
                 }
-                if(i+1 < testnumber && j-1 >0 && mapp[i][j] !='B')
+                if(i+1 < maplen && j-1 >=0 && mapp[i+1][j-1] !='B')
                 {
                    start[i][j]+=start[i+1][j-1];
                 }
-                if(i+1 < testnumber && j+1 <testnumber && mapp[i][j] =='B')
+                if(i+1 < maplen && j+1 <maplen && mapp[i+1][j+1] =='B')
                 {
-                   if(i+2 < testnumber && j+2 <testnumber && mapp[i+2][j+2] !='B')
+                   if(i+2 < maplen && j+2 <maplen && mapp[i+2][j+2] !='B')
                    {
                       start[i][j]+=start[i+2][j+2];
                    }
                 }
-                if(i+1 < testnumber && j-1 >=0 && mapp[i+1][j-1] =='B')
+                if(i+1 < maplen && j-1 >=0 && mapp[i+1][j-1] =='B')
                 {
-                   if(i+2 < testnumber && j-2 >=0 && mapp[i+2][j-2] !='B')
+                   if(i+2 < maplen && j-2 >=0 && mapp[i+2][j-2] !='B')
                    {
                       start[i][j]+=start[i+2][j-2];
                    }
@@ -68,10 +74,13 @@ int main()
             }
         }
          int ans = 0;
-        for(i = 0; i < n; i++)
-            if(mapp[0][i] != 'B')
+        for(i = 0; i < maplen; i++){
+            if(mapp[0][i] != 'B'){
                 ans += start[0][i], ans %= 1000007;
+			}
+		}
         printf("Case %d: %d\n", ++cases, ans);
     }
     return 0;
+	//system("pause");
 }
