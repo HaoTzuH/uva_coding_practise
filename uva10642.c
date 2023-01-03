9f15981d563d53aaa1a88402262d3eccae93c7be
HaoTzuH <119925963+HaoTzuH@users.noreply.github.com>
2023-01-03 20:25:09 +0800

    Create uva10642.c

---
+++ uva10642.c
@@ -0,0 +1,31 @@
+/*#include <stdio.h>
+
+
+long loc(int x , int y)
+{
+   return ((x+y)*(x+y+1))/2 +x;//
+}
+
+int main()
+{
+
+  int	 inputcase = 0;
+  int	 startx = 0, starty = 0, denx = 0, deny = 0;
+  int	 stepcount= 0;
+  int	 i = 1;
+  scanf("%d", &inputcase);
+  if(inputcase > 500 || inputcase < 0)
+  {
+     return 0;
+  }
+  
+  while(inputcase--)
+  {
+      scanf("%d%d%d%d", &startx, &starty, &denx, &deny);
+      stepcount=loc(denx,deny)- loc(startx,starty);//2653768670
+      //(denx+deny)*(denx+deny+1)
+      
+      printf("Case %d: %d\n",i,stepcount);
+      i++;
+  }
+}*/
