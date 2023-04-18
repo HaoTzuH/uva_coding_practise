5943f8ff4b142b3b041f1f160d6fe97cceb4b87a
HaoTzuH <119925963+HaoTzuH@users.noreply.github.com>
2023-04-18 16:21:18 +0800

    Update uva10642.c

--- uva10642.c
+++ uva10642.c
@@ -1,34 +1,24 @@
-/*
-超過int範圍未解
-
 #include <stdio.h>
-
-
-long loc(int x , int y)
-{
-   return ((x+y)*(x+y+1))/2 +x;//
-}
 
 int main()
 {
-
-  int	 inputcase = 0;
-  int	 startx = 0, starty = 0, denx = 0, deny = 0;
-  int	 stepcount= 0;
-  int	 i = 1;
-  scanf("%d", &inputcase);
-  if(inputcase > 500 || inputcase < 0)
-  {
-     return 0;
-  }
-  
-  while(inputcase--)
-  {
-      scanf("%d%d%d%d", &startx, &starty, &denx, &deny);
-      stepcount=loc(denx,deny)- loc(startx,starty);//2653768670
-      //(denx+deny)*(denx+deny+1)
-      
-      printf("Case %d: %d\n",i,stepcount);
-      i++;
-  }
-}*/
+    int sourcex, sourcey ,desx ,desy;
+    int i = 0,testcase = 0, j = 0;
+    long long int step;
+    int sourt = 0, destotal = 0;
+    scanf("%d", &testcase);
+    while(testcase--)
+    {
+        scanf("%d %d %d %d",&sourcex,&sourcey,&desx,&desy);
+        step = 0;
+        sourt = sourcex+sourcey;
+        destotal = desx+desy;
+        for(i = sourt+1 ; i <= destotal ;i++)
+        {
+            step = i +step;
+        }
+        step = step - sourcex + desx;
+        j++;
+        printf("Case %d: %d\n",j,step);
+    }
+}
